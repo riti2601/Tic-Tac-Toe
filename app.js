c6da488c550c26fc06d52cdfc4c5401a1d8df397
riti2601 <ritishapande@gmail.com>
2024-07-03 00:44:38 +0530

    initial

---
+++ app.js
@@ -0,0 +1,15 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelectorAll(".reset-btn");
+
+let turnO = true; //playerX, playerO
+
+const winPattern = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
